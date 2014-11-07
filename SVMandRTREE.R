a4a52ae13f6e3d2fa73a1f3ed5f56b9acee961bf
Avnish Kumar <avnish.10.chd@gmail.com>
2014-11-06 22:39:11 -0500

    first commit- should have done a long time ago

---
+++ SVMandRTREE.R
@@ -0,0 +1,100 @@
+#data1 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_1_s1data.csv', header=TRUE)#stringsAsFactors = FALSE
+#data2 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_1_s2data.csv', header=TRUE)#stringsAsFactors = FALSE
+#data3 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_2_s1data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data4 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_2_s2data.csv', header=TRUE)#stringsAsFactors = FALSE
+#data5 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_3_s1data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data6 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_3_s2data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data7 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_4_s1data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data8 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_4_s2data.csv', header=TRUE)#stringsAsFactors = FALSE
+data9 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_5_s3data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data10 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_5_s4data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data11 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_6_s3data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data12 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_6_s4data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data13 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_7_s3data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data14 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_7_s4data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data15 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_8_s3data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data16 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_8_s4data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data17 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_9_s1data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data18 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_9_s2data.csv', header=TRUE)#stringsAsFactors = FALSE
+#data19 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_10_s1data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data20 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_10_s2data.csv', header=TRUE)#stringsAsFactors = FALSE
+# 
+#data21 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_12_s1data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data22 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_12_s2data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data23 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_14_s1data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data24 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_14_s2data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data25 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_15_s1data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data26 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_15_s2data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data27 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_16_s3data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data28 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_16_s4data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data29 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_17_s3data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data30 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_17_s4data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data31 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_19_s3data.csv', header=TRUE)#stringsAsFactors = FALSE
+#data32 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_19_s4data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data33 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_20_s1data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data34 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_20_s2data.csv', header=TRUE)#stringsAsFactors = FALSE
+#data35 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_21_s1data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data36 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_21_s2data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data37 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_22_s3data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data38 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_22_s4data.csv', header=TRUE)#stringsAsFactors = FALSE
+# data39 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_23_s3data.csv', header=TRUE)#stringsAsFactors = FALSE
+#data40 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_23_s4data.csv', header=TRUE)#stringsAsFactors = FALSE
+
+#P1s1<-rbind(data9,data21)#,data3,data4,data5,data6,data7,data8,data9,data10)#,data11,data12,data13,data14,data15,data16,data17,data18,data19,data20,data21,data22,data23,data24,data25,data26,data27,data28,data29,data30,data31,data32,data33,data34,data35,data36,data37,data38,data39,data40)
+P1s1<- data.frame(data9, check.rows=TRUE)
+p1s1<-P1s1[,c(9,26:27,30:40, 46, 48, 57, 59, 68, 70)]
+p1s1[p1s1 ==""]<- NA
+p1s1<-p1s1[,colSums(is.na(p1s1))<nrow(p1s1)] #remove column with all NA
+p1s1<-p1s1[complete.cases(p1s1),] #remove rows with NA; Not yet...try to ignore na in the algo step
+#p1s1<-p1s1[na.omit(p1s1),]
+
+# Ptest<-data.frame(data32)
+# ptest<-Ptest[,c(9,26:27,30:40, 46, 48, 57, 59, 68, 70)]
+# ptest[ptest ==""]<- NA
+# ptest<-ptest[,colSums(is.na(ptest))<nrow(ptest)] #remove column with all NA
+# #ptest<-ptest[complete.cases(ptest),] #remove rows with NA; Not yet...try to ignore na in the algo step
+# ptest<-ptest[na.omit(ptest),]
+
+## split data into a train and test set
+index <- 1:nrow(p1s1)
+testindex <- sample(index, trunc(length(index)/3))#randomly choose one-third p1s1 rows to be in the test-set
+trainset <- p1s1[-testindex,]
+testset <- p1s1[testindex,]
+#testsetNew<-ptest[testindex,]
+
+# trainset$Action<-as.numeric(as.character(trainset$Action))
+# trainset$missionTime<-as.character(trainset$missionTime)
+# trainset$segment<-as.character(trainset$segment)
+# trainset$Closest_Screen<-as.character(trainset$Closest_Screen)
+# trainset$Closest_Control<-as.character(trainset$Closest_Control)
+
+# trainset$Session<-as.numeric(trainset$Session)
+# trainset$longTime<-as.numeric(trainset$longTime)
+# trainset$Block<-as.numeric(trainset$Block)
+# trainset$ScoreChange<-as.numeric(trainset$ScoreChange)
+# trainset$score<-as.numeric(trainset$score)
+
+## svm
+library('e1071')
+svm.model <- svm(Difficulty ~., data=trainset, cost=10, gamma=.1)#, na.action=na.omit)
+svm.pred <- predict(svm.model, testset[,-1], na.action=na.omit)# -6
+#svm.pred <- predict(svm.model, testsetNew[,-1])#, na.action=na.omit)# -6
+
+## rpart
+library('rpart')
+rpart.model <- rpart(Difficulty ~., data=trainset)
+rpart.pred <- predict(rpart.model, testset[,-1], type = "class")
+#rpart.pred <- predict(rpart.model, testsetNew[,-1], type = "class")
+
+## compute SVM confusion matrix
+#a.svm<-table(pred = svm.pred, true = testset[,1])
+a.svm<-table(pred = svm.pred, true = testset[,1])
+a.svm
+CCR.svm<-(a.svm[1,1]+a.svm[2,2]+a.svm[3,3])/(a.svm[1,1]+a.svm[1,2]+a.svm[1,3]+a.svm[2,1]+a.svm[2,2]+a.svm[2,3]+a.svm[3,1]+a.svm[3,2]+a.svm[3,3])
+CCR.svm
+## compute rpart confusion matrix
+a.rpart<-table(pred = rpart.pred, true = testset[,1])
+#a.rpart<-table(pred = rpart.pred, true = testset[,1])
+a.rpart
+CCR.rpart<-(a.rpart[1,1]+a.rpart[2,2]+a.rpart[3,3])/(a.rpart[1,1]+a.rpart[1,2]+a.rpart[1,3]+a.rpart[2,1]+a.rpart[2,2]+a.rpart[2,3]+a.rpart[3,1]+a.rpart[3,2]+a.rpart[3,3])
+CCR.rpart
---
+++ preProcessing.R
@@ -0,0 +1,48 @@
+library(MASS)
+library(ISLR)
+P1s1 <- read.csv('/home/avnish/R/my_data/Dataset2/Exp 1 Data - 20 Subjects/P_22_s4data.csv', header=TRUE)#stringsAsFactors = FALSE
+#modify data
+#P1s1 <- P1s1[-which(P1s1$Action == ""), ] #only action rows=""
+#P1s1 <- P1s1[-which(P1s1$ScoreChange !=0), ]
+P1s1<- data.frame(P1s1, check.rows=TRUE)
+P1s1<-P1s1[,-29] #delete 'Closest_Control' column
+P1s1<-P1s1[P1s1$Action =="", ] #delete all non-empty 'Action' column
+#column 'eventType', 'MsgIn', 'msgOut', 'Action':blank-->"inf" {type=string}
+P1s1$eventType<-as.character(P1s1$eventType)
+P1s1$eventType[P1s1$eventType ==""]<-"inf"
+P1s1$eventType<-as.factor(P1s1$eventType)
+
+P1s1$MsgIn<-as.character(P1s1$MsgIn)
+P1s1$MsgIn[P1s1$MsgIn ==""]<-"inf"
+P1s1$MsgIn<-as.factor(P1s1$MsgIn)
+
+P1s1$MsgOut<-as.character(P1s1$MsgOut)
+P1s1$MsgOut[P1s1$MsgOut ==""]<-"inf"
+P1s1$MsgOut<-as.factor(P1s1$MsgOut)
+
+P1s1$Action<-as.character(P1s1$Action)
+P1s1$Action[P1s1$Action ==""]<-"inf"
+P1s1$Action<-as.factor(P1s1$Action)
+
+#column 'ScoreChange':blank/NA-->"-123"; 0 remains 0 {type=numeric}
+P1s1$ScoreChange[P1s1$ScoreChange ==""]<- 123 #just in case there are blanks
+P1s1$ScoreChange[is.na(P1s1$ScoreChange)]<- 123
+#check
+#P1s1$ScoreChange<-as.factor(P1s1$ScoreChange)
+#levels(P1s1$ScoreChange)
+
+#rest of the missing/NA data must be NaN
+#for <NA> type, factor-->character-->(NA-->NaN)-->factor
+for (i in names(P1s1)[c(28,41,42,50,52,53,61,63,64,72,74,80,81,82,88,89,90,96,97,98,104,105,106,112,113,114,120,121,122,128,129,130,136,137,138,144,145,146,152,154,154,160,161,162,168,169,170,176,177,178,184,185,186,192,193)]){
+  P1s1[[i]]<-as.character(P1s1[[i]])
+  P1s1[[i]][is.na(P1s1[[i]])]<-NaN
+  P1s1[[i]][P1s1[[i]]==""]<-NaN
+  P1s1[[i]]<-as.factor(P1s1[[i]])
+}
+
+#rest(numeric class) of the NA data
+P1s1[is.na(P1s1)]<-NaN
+P1s1[P1s1==""]<-NaN
+
+# Write CSV in R
+write.csv(P1s1, file = "newP_22_s4data.csv", na = "NaN", row.names=FALSE, quote = FALSE)
